fe53af6c209c674a7ceb85db401e1a2c926907f6
Igor Makarov <igormaka@gmail.com>
2020-09-27 11:50:20 +0300

    add all the build scripts locally

---
+++ Scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eo pipefail
+
+mkdir -p _specs/_site || true
+
+# generate sharded index
+bundle exec ruby Scripts/create_pods_and_versions_index.rb 
+
+cp _specs/*.yml _specs/_site/
